16979e779f870bb9fd94cf67336535701154f22a
Wanderson Sena <senawandershow@gmail.com>
2020-09-07 16:42:51 -0300

    Criando um app Django

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wanderson sena\desktop\tdd-lab1\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
